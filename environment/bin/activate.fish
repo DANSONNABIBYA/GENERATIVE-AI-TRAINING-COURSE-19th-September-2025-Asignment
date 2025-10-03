1bf1a12fd42fafe04c12f9474a5322c43b1d2365
dansonnabibya <danabibya1@outlook.com>
2025-10-03 20:16:09 +0300

    Assignment update 4

--- environment/bin/activate.fish
+++ environment/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/dansonnabibya/environment/GENERATIVE-AI-TRAINING-COURSE-19th-September-2025-Asignment/environment
+set -gx VIRTUAL_ENV /home/dansonnabibya/GENERATIVE-AI-TRAINING-COURSE-19th-September-2025-Asignment/environment
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
